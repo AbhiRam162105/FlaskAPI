93ed083de4314036b975a0444a58fdd1544b65be
AbhiRam162105 <abhiyanampally@gmail.comgit>
2023-12-16 15:03:19 +0530

    CORS and generation API Added

--- apis.py
+++ apis.py
@@ -1,63 +1,52 @@
-
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS  # Import CORS
 import requests
 
+headers = {"Authorization": "Bearer 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"}
+
 # creating the flask app
 app = Flask(__name__)
+
 # creating an API object
 api = Api(app)
-CORS(app, resources={r"/*": {"origins": "*"}})
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
-# other methods include put, delete, etc.
-''' 
-"inputs": "content"
-'''
+# Allow all origins, adjust as needed
+CORS(app, resources={r"/": {"origins": "*"}})
 
 
 class T5_small(Resource):
-    def post(self):
+    @staticmethod
+    def post():
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {
-            "Authorization": "Bearer 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"}
         payload = request.get_json()
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
 
 
-# '''
-# "inputs": {
-# 	"question": "What is my name?",
-# 	"context": "My name is Clara and I live in Berkeley."
-#   },
-# '''
 class qna(Resource):
-    def post(self):
+    @staticmethod
+    def post():
         API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
-        headers = {
-            "Authorization": "Bearer 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"}
         payload = request.get_json()
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
-
-# '''
-# {
-#     "inputs": "My name is Sarah Jessica Parker but you can call me Jessica", "parameters": {"src_lang": "en_XX", "tgt_lang": "fr_XX"}
-# }
-# '''
 
 
 class translate(Resource):
-    def post(self):
+    @staticmethod
+    def post():
         API_URL = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt"
-        headers = {
-            "Authorization": "Bearer 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"}
         payload = request.get_json()
         response = requests.post(API_URL, headers=headers, json=payload)
-        print(response)
+        return response.json()
+
+
+class text_generation(Resource):
+    @staticmethod
+    def post():
+        API_URL = "https://api-inference.huggingface.co/models/gpt2"
+        payload = request.get_json()
+        response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
 
 
@@ -65,8 +54,8 @@ class translate(Resource):
 api.add_resource(T5_small, '/summarize')
 api.add_resource(qna, '/qna')
 api.add_resource(translate, '/translate')
+api.add_resource(text_generation, '/text_generation')
 
 # driver function
 if __name__ == '__main__':
-
     app.run(debug=False)
